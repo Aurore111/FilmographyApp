827a212330ce6d99a5b32ee764cff2a7f3a55f0a
sihamlgh <sihaml16.laghrib@gmail.com>
2026-03-09 14:22:27 +0100

    list film database

--- app/src/main/java/fr/isen/aurore/filmographyapp/ListFilm.kt
+++ app/src/main/java/fr/isen/aurore/filmographyapp/ListFilm.kt
@@ -3,26 +3,15 @@ package fr.isen.aurore.filmographyapp
 import android.content.Intent
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -30,68 +19,137 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.firebase.database.FirebaseDatabase
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ListFilm (modifier: Modifier) {
-    //  On remplace le brute avec l'appel API
-    val context = LocalContext.current //pour la fleche retour en arriere dans la topBar
-    val franchises = listOf("La Menace Fantome", "L'attaque des Clones", "La revanche des Sith", "Un nouvel espoir", "l'empire contre attaque", "Le retour du Jedi")
+fun ListFilm(modifier: Modifier, categoryName: String?) {
 
-    Scaffold (
+    val context = LocalContext.current
+    val films = remember { mutableStateListOf<String>() }
+
+    val database = FirebaseDatabase.getInstance("https://filmographyapp-8fb1e-default-rtdb.europe-west1.firebasedatabase.app")
+    val ref = database.getReference("categories")
+
+    LaunchedEffect(categoryName) {
+
+        ref.get().addOnSuccessListener { snapshot ->
+
+            films.clear()
+
+            snapshot.children.forEach { category ->
+
+                val nomCategorie = category.child("categorie").value.toString()
+
+                if (nomCategorie == categoryName) {
+
+                    category.child("franchises").children.forEach { franchise ->
+
+                        // films avec sous sagas
+                        franchise.child("sous_sagas").children.forEach { saga ->
+
+                            saga.child("films").children.forEach { film ->
+
+                                val titre = film.child("titre").value.toString()
+                                films.add(titre)
+
+                            }
+
+                        }
+
+                        // films sans sous saga
+                        franchise.child("films").children.forEach { film ->
+
+                            val titre = film.child("titre").value.toString()
+                            films.add(titre)
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+    Scaffold(
+
         topBar = {
             CenterAlignedTopAppBar(
+
                 title = {
                     Text(
-                        text = "Star Wars",
+                        text = categoryName ?: "Films",
                         fontWeight = FontWeight.ExtraBold,
                         color = Color(0xFF3E2723)
                     )
                 },
-                navigationIcon = {   //fleche retour à la page précédente
-                    IconButton (onClick = { (context as? ComponentActivity)?.finish() }) {
+
+                navigationIcon = {
+                    IconButton(onClick = { (context as? ComponentActivity)?.finish() }) {
                         Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Retour")
                     }
                 }
+
             )
         }
+
     ) { innerPadding ->
+
         LazyVerticalGrid(
+
             columns = GridCells.Fixed(2),
+
             modifier = modifier
                 .fillMaxSize()
                 .background(Color(0xFFFCCFA4))
                 .padding(innerPadding)
                 .padding(16.dp),
+
             horizontalArrangement = Arrangement.spacedBy(12.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp)
+
         ) {
-            items(franchises) { univers ->
-                val context = LocalContext.current
+
+            items(films) { film ->
 
                 Button(
+
                     onClick = {
                         val intent = Intent(context, FilmDescriptionActivity::class.java)
-                        intent.putExtra("Film", univers)
+                        intent.putExtra("Film", film)
                         context.startActivity(intent)
-                              },
+                    },
+
                     modifier = Modifier
                         .fillMaxWidth()
                         .aspectRatio(1.1f),
+
                     shape = RoundedCornerShape(20.dp),
+
                     colors = ButtonDefaults.buttonColors(
                         containerColor = Color.White.copy(alpha = 0.8f),
                         contentColor = Color(0xFF5D4037)
                     )
+
                 ) {
+
                     Text(
-                        text = univers,
+                        text = film,
                         fontSize = 20.sp,
                         textAlign = TextAlign.Center,
                         fontWeight = FontWeight.Bold
                     )
+
                 }
+
             }
+
         }
+
     }
+
 }
